b97869eb2a1fc08e8944bdcf0cbd669ab9d76dd5
Marius Constantin <68522001+realmariusconstantin@users.noreply.github.com>
2024-11-23 15:21:03 +0000

    added menu to navigation bar

---
+++ src/components/Molecules/navbarContent.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+<MenuWhite />
+    <div class="signup">
+      <router-link to="/signup">
+        <img src="@/assets/Icons/others/Sign%20in.png" alt="signup" />
+      </router-link>
+    </div>
+
+  </nav>
+</template>
+
+<script>
+import MenuWhite from "@/components/atoms/MenuWhite.vue";
+
+export default {
+  components: {
+    MenuWhite,
+  },
+};
+</script>
+
+<style>
+
+//Navigation styles
+nav {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  gap: 20px;
+}
+
+//Signup button styles
+.signup img {
+  cursor: pointer;
+}
+
+</style>
